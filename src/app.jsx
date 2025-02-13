af1e6dfc720b38b9a22c9b773df5f70c3443eb70
Jaiden <jmt268@byu.edu>
2025-02-12 20:24:29 -0700

    Updated app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -2,37 +2,55 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { AllRecipes } from './allRecipes/allRecipes';
+import { RecipesForYou } from './recipesForYou/recipesForYou';
+import { About } from './about/about';
+
 export default function App() {
   return (
-    <div className="body bg-dark text-light">
-        <header id = "header" className="container-fluid">
-            <nav id = "nav-bar" className= "navbar fixed-top">
-            <a className="navbar-brand" href="#">Simple Supper<sup>&reg;</sup></a>
-                <menu className="navbar-nav">
-                    <li className="nav-item">
-                        <a className="nav-link active" href="index.html">Home</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link active" href="recipesForYou.html">Recipes for You</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link active" href="allRecipes.html">All Recipes</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link active" href="about.html">About</a>
-                    </li>
-                </menu>
-            </nav>
-        </header>
+    <BrowserRouter>
+        <div className="body bg-dark text-light">
+            <header id = "header" className="container-fluid">
+                <nav id = "nav-bar" className= "navbar fixed-top">
+                <div className="navbar-brand" href="#">Simple Supper<sup>&reg;</sup></div>
+                    <menu className="navbar-nav">
+                        <li className="nav-item">
+                            <NavLink className="nav-link active" to="index">Home</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link active" to="recipesForYou">Recipes for You</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link active" to="allRecipes">All Recipes</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link active" to="about">About</NavLink>
+                        </li>
+                    </menu>
+                </nav>
+            </header>
 
-        <main className="container-fluid bg-light text-center">App components go here</main>
+            <Routes>
+                <Route path='/' element={<Login />} exact />
+                <Route path='/allRecipes' element={<AllRecipes />} />
+                <Route path='/recipesForYou' element={<RecipesForYou />} />
+                <Route path='/about' element={<About />} />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
 
-        <footer className="bg-dark text-white-50">
-            <div className="container-fluid">
-                <span className="text-reset">Jaiden Tripp</span>
-                <a className="text-reset" href="https://github.com/jaidentripp/CS260StartupProject">GitHub</a>
-            </div>
-        </footer>
-    </div>
+            <footer className="bg-dark text-white-50">
+                <div className="container-fluid">
+                    <span className="text-reset">Jaiden Tripp</span>
+                    <NavLink className="text-reset" href="https://github.com/jaidentripp/CS260StartupProject">GitHub</NavLink>
+                </div>
+            </footer>
+        </div>
+    </BrowserRouter>
   )
 }
+
+function NotFound() {
+    return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
+  }
