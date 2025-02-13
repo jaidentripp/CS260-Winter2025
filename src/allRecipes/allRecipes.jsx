cdb3c2cca9432b8ac3b43c5e23e0ae4966751100
Jaiden <jmt268@byu.edu>
2025-02-12 20:14:44 -0700

    Added jsx files to each page

---
+++ src/allRecipes/allRecipes.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function AllRecipes() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>AllRecipes displayed here</div>
+    </main>
+  );
+}
---
+++ src/recipesForYou/recipesForYou.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function RecipesForYou() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>RecipesForYou displayed here</div>
+    </main>
+  );
+}
