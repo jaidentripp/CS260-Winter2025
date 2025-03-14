4c096ca3bdab92fd9dc73e0b901465bd845c293d
Jaiden <jmt268@byu.edu>
2025-03-13 22:35:36 -0600

    Added react to allRecipes - not working

--- src/allRecipes/allRecipes.jsx
+++ src/allRecipes/allRecipes.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './allRecipes.css';
 
 export function AllRecipes() {
+  const [recipies, setRecipes] = useState([]);
+
+  useEffect(() => {
+    fetchRecipes();
+  }, []);
+
+  const fetchRecipes = async () => {
+    //Simulating API call to fetch recipies
+    //*replace with API call
+    const mockRecipes = [
+      { id: 1, image: 'placeholder.jpg', alt: 'recipe1' },
+      { id: 2, image: 'placeholder.jpg', alt: 'recipe2' },
+      { id: 3, image: 'placeholder.jpg', alt: 'recipe3' },
+    ];
+
+    setTimeout(() => {
+      setRecipes(mockRecipes);
+    }, 1000);
+  };
+
   return (
     <main className="container-fluid bg-light text-center">
       <table>
@@ -11,7 +31,14 @@ export function AllRecipes() {
           </tr>
         </thead>
         <tbody>
-          <tr>
+          {recipies.map((recipie) => (
+            <tr key={recipe.id}>
+              <td>
+                <img width="400px" src={recipe.image} alt={recipe.alt} />
+              </td>
+            </tr>
+          ))}
+          {/* <tr>
             <td> <img width="400px" src="placeholder.jpg" alt="recipe1"/> </td>
           </tr>
           <tr>
@@ -19,7 +46,7 @@ export function AllRecipes() {
           </tr>
           <tr>
             <td> <img width="400px" src="placeholder.jpg" alt="recipe3"/> </td>
-          </tr>
+          </tr> */}
         </tbody>
       </table>
     </main>
