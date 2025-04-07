66f40db9b95a687a7ef371f9e5798ab45023cc14
Jaiden <jmt268@byu.edu>
2025-04-07 11:11:39 -0600

    Added restricted endpoint

--- src/app.jsx
+++ src/app.jsx
@@ -11,35 +11,52 @@ import { AuthState } from './login/authState';
 import { useState } from 'react';
 
 export default function App() {
-    const [userName, setUserName] = useState('');
-    const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
-    const [authState, setAuthState] = React.useState(currentAuthState);
+    // const [userName, setUserName] = useState('');
+    // const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
+    // const [authState, setAuthState] = React.useState(currentAuthState);
+
+    const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
+  const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
+  const [authState, setAuthState] = React.useState(currentAuthState);
 
   return (
     <BrowserRouter>
-        <div className="body bg-dark text-light">
-            <header id = "header" className="container-fluid">
-                <nav id = "nav-bar" className= "navbar fixed-top">
-                <div className="navbar-brand">Simple Supper<sup>&reg;</sup></div>
-                {/* Display user's name */}
-                {userName && <span className="text-light">Welcome {userName}!</span>} 
-                    <menu className="navbar-nav">
-                        <li className="nav-item">
-                            <NavLink className="nav-link active" to="/">Home</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link active" to="recipesForYou">Recipes for You</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link active" to="allRecipes">All Recipes</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link active" to="about">About</NavLink>
-                        </li>
-                    </menu>
-                </nav>
-            </header>
+        <div className='body bg-dark text-light'>
+        <header className='container-fluid'>
+          <nav id = "nav-bar" className= "navbar fixed-top">
+            <div className='navbar-brand'>
+              Simple Supper<sup>&reg;</sup>
+            </div>
+            <menu className='navbar-nav'>
+              <li className='nav-item'>
+                <NavLink className='nav-link' to=''>
+                  Login
+                </NavLink>
+              </li>
+              {authState === AuthState.Authenticated && (
+                <li className='nav-item'>
+                  <NavLink className='nav-link' to='recipesForYou'>
+                    Recipes For You
+                  </NavLink>
+                </li>
+              )}
+              {authState === AuthState.Authenticated && (
+                <li className='nav-item'>
+                  <NavLink className='nav-link' to='allRecipes'>
+                    All Recipes
+                  </NavLink>
+                </li>
+              )}
+              <li className='nav-item'>
+                <NavLink className='nav-link' to='about'>
+                  About
+                </NavLink>
+              </li>
+            </menu>
+          </nav>
+        </header>
 
+            
             <Routes>
                 <Route
                     path='/'
@@ -76,3 +93,27 @@ export default function App() {
 function NotFound() {
     return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
   }
+
+
+//   <div className="body bg-dark text-light">
+//             <header id = "header" className="container-fluid">
+//                 <nav id = "nav-bar" className= "navbar fixed-top">
+//                 <div className="navbar-brand">Simple Supper<sup>&reg;</sup></div>
+//                 {/* Display user's name */}
+//                 {userName && <span className="text-light">Welcome {userName}!</span>} 
+//                     <menu className="navbar-nav">
+//                         <li className="nav-item">
+//                             <NavLink className="nav-link active" to="/">Home</NavLink>
+//                         </li>
+//                         <li className="nav-item">
+//                             <NavLink className="nav-link active" to="recipesForYou">Recipes for You</NavLink>
+//                         </li>
+//                         <li className="nav-item">
+//                             <NavLink className="nav-link active" to="allRecipes">All Recipes</NavLink>
+//                         </li>
+//                         <li className="nav-item">
+//                             <NavLink className="nav-link active" to="about">About</NavLink>
+//                         </li>
+//                     </menu>
+//                 </nav>
+//             </header> 
